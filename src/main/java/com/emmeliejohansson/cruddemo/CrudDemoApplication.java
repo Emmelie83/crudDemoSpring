f809695bbaa86645e4abcda070b2d7a36c2f8378
Emmelie83 <info@emmeliejohansson.com>
2024-03-21 02:17:24 +0100

    Add update table

--- src/main/java/com/emmeliejohansson/cruddemo/CrudDemoApplication.java
+++ src/main/java/com/emmeliejohansson/cruddemo/CrudDemoApplication.java
@@ -21,13 +21,13 @@ public class CrudDemoApplication {
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
             //createStudent(studentDAO);
-            //createMultipleStudents(studentDAO);
+            createMultipleStudents(studentDAO);
             //readStudent(studentDAO);
             //queryForStudents(studentDAO);
             //queryForStudentsByLastName(studentDAO);
             //updateStudent(studentDAO);
             //deleteStudent(studentDAO);
-            deleteAll(studentDAO);
+            //deleteAll(studentDAO);
         };
     }
 
